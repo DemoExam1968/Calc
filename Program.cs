26d324a9f738b11b60dceba915e29e6fb9d5ccdd
Ирина Матысик <irinamatisik1968@gmail.com>
2024-10-10 16:07:01 +0300

    Вычитание двух чисел

--- Program.cs
+++ Program.cs
@@ -10,13 +10,15 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            int a, b, resultAdd;
+            int a, b, resultAdd, resultSub;
             Console.WriteLine("Введите первое число ");
             a=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите второе число ");
             b = Convert.ToInt32(Console.ReadLine());
             resultAdd = a+b;
             Console.WriteLine("Сумма = "+resultAdd);
+            resultSub = a - b;
+            Console.WriteLine("Разность = " + resultSub);
             Console.ReadKey();
         }
     }
